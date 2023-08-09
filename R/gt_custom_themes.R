425c611f559a74fdea975adef309d3f082fa6168
Mohit Shrestha <mohitshrestha@gmail.com>
2023-08-09 13:38:47 -0700

    Aug 9 2023: Updated description and examples for gt_custom_themes function and ms_brand_palette function

--- R/gt_custom_themes.R
+++ R/gt_custom_themes.R
@@ -3,6 +3,38 @@
 #' Customize Table Theme with the gt Package.
 #'
 #' @export
+#'
+#' @examples
+#' library(automaton)
+#' library(tidyverse)
+#' library(gt)
+#'
+<<<<<<< HEAD
+<<<<<<< HEAD
+#' # Assign penguins data set to tbl data frame 'tbl'
+#' tbl <- penguins
+#'
+#' # Convert data frame to gt table format
+#' gt_table <- tbl |> gt()
+#'
+#' # Apply customized gt theme
+#' gt_table |> gt_theme_ms()
+=======
+=======
+>>>>>>> 3771dc7000c54237e34c99c812a36266aa80d7e6
+#' Assign penguins data set to tbl data frame 'tbl'
+#' tbl <- penguins
+#' # Convert data frame to gt table format
+#' gt_table <- tbl |> gt()
+
+#' # Apply customized gt theme
+#' gt_table |> gt_theme_ms()
+#' @family Utilities
+#'
+<<<<<<< HEAD
+>>>>>>> 3771dc7000c54237e34c99c812a36266aa80d7e6
+=======
+>>>>>>> 3771dc7000c54237e34c99c812a36266aa80d7e6
 
 gt_theme_ms <- function(gt_tbl) {
 
@@ -13,14 +45,14 @@ gt_theme_ms <- function(gt_tbl) {
         tab_options(
             data_row.padding = px(6),
             heading.align = 'left',
-            column_labels.background.color = 'dodgerblue4',
+            column_labels.background.color = ms_brand_palette("main")[length(ms_brand_palette("main"))],
             heading.title.font.size = px(26),
             heading.subtitle.font.size = px(14),
             table_body.hlines.width = px(0)
         ) |>
         tab_style(
             style = cell_text(
-                color = 'dodgerblue4',
+                color = ms_brand_palette("main")[length(ms_brand_palette("main"))],
                 weight = 'bold',
                 font = google_font('Merriweather')
             ),
--- R/palattecode.R
+++ R/palattecode.R
@@ -1,3 +1,20 @@
+#' Use Custom Brand Color Palette.
+#'
+#' @export
+#'
+#' @examples
+<<<<<<< HEAD
+<<<<<<< HEAD
+#' library(automaton)
+#' library(scales)
+=======
+>>>>>>> 3771dc7000c54237e34c99c812a36266aa80d7e6
+=======
+>>>>>>> 3771dc7000c54237e34c99c812a36266aa80d7e6
+#' #The `show_col` function from the `scales` package is a nifty way to showcase all the colors available in a given palette:
+#' scales::show_col(ms_brand_palette("main"), cex_label = 1)
+#' scales::show_col(ms_brand_palette("highlight"), cex_label = 1)
+
 ms_brand_palette <- function(palette = "main", ...) {
 
     ms_brand_palettes <- list(
@@ -11,6 +28,4 @@ ms_brand_palette <- function(palette = "main", ...) {
 
 }
 
-# The `show_col` function from the `scales` package is a nifty way to showcase all the colors available in a given palette:
-## scales::show_col(ms_brand_palette("main"), cex_label = 1)
-## scales::show_col(ms_brand_palette("highlight"), cex_label = 1)
+
