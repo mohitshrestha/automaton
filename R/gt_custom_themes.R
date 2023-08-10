82425e73a7190230ee467e3cf285f6128fb9f6cd
Mohit Shrestha <mohitshrestha@gmail.com>
2023-08-10 12:03:13 -0700

    Aug 10 2023: Updated description and examples for gt_custom_themes function

--- R/gt_custom_themes.R
+++ R/gt_custom_themes.R
@@ -1,10 +1,21 @@
-#' Use Custom GT theme.
+#' Customize GT table
 #'
-#' Customize Table Theme with the gt Package.
+#' @description Customize Table Theme with the gt Package.
 #'
+#' @param gt_tbl Input GT table not just data frame. First convert data frame/tibble to GT table then input that gt table to the function
+#'
+#' @return Returns the gt table using the customized theme
 #' @export
+#'
+#' @examples
+#' library(automaton)
+#' library(gt)
+#' tbl <- automaton::penguins
+#' gt_tbl <- tbl |> gt()
+#'
+#' gt_tbl |> gt_theme_ms()
 
-gt_theme_ms <- function(gt_tbl) {
+gt_theme_ms <- function(gt_tbl = automaton::penguins |> gt()) {
 
     # Grab number of rows of data from gt object
     n_rows <- nrow(gt_tbl$`_data`)
@@ -13,14 +24,14 @@ gt_theme_ms <- function(gt_tbl) {
         tab_options(
             data_row.padding = px(6),
             heading.align = 'left',
-            column_labels.background.color = 'dodgerblue4',
+            column_labels.background.color = ms_brand_palette("main")[length(ms_brand_palette("main"))],
             heading.title.font.size = px(26),
             heading.subtitle.font.size = px(14),
             table_body.hlines.width = px(0)
         ) |>
         tab_style(
             style = cell_text(
-                color = 'dodgerblue4',
+                color = ms_brand_palette("main")[length(ms_brand_palette("main"))],
                 weight = 'bold',
                 font = google_font('Merriweather')
             ),
