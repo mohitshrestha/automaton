26e570e461554ec787f38d152ee1cdcbf1db238e
Mohit Shrestha <mohitshrestha@gmail.com>
2023-08-10 16:25:50 -0700

    Aug 10 2023: Fixed gt_custom_themes function to either take data frame or Gt table object. Included in the function to convert to gt table object accordingly.

--- R/gt_custom_themes.R
+++ R/gt_custom_themes.R
@@ -11,35 +11,44 @@
 #' library(automaton)
 #' library(gt)
 #' tbl <- automaton::penguins
-#' gt_tbl <- tbl |> gt()
+#' gt_tbl <- tbl |>
+#'             dplyr::count(species) |>
+#'             gt()
 #'
 #' gt_tbl |> gt_theme_ms()
 
-gt_theme_ms <- function(gt_tbl = automaton::penguins |> gt()) {
+gt_theme_ms <- function(data = automaton::penguins) {
+
+    # Ensure data entered is a gt table object
+    if (is.data.frame(data) == TRUE) {
+        gt_tbl <- data |> gt::gt()
+    } else {
+        gt_tbl <- data
+    }
 
     # Grab number of rows of data from gt object
     n_rows <- nrow(gt_tbl$`_data`)
 
     gt_tbl |>
-        tab_options(
-            data_row.padding = px(6),
+        gt::tab_options(
+            data_row.padding = gt::px(6),
             heading.align = 'left',
             column_labels.background.color = ms_brand_palette("main")[length(ms_brand_palette("main"))],
-            heading.title.font.size = px(26),
-            heading.subtitle.font.size = px(14),
-            table_body.hlines.width = px(0)
+            heading.title.font.size = gt::px(26),
+            heading.subtitle.font.size = gt::px(14),
+            table_body.hlines.width = gt::px(0)
         ) |>
-        tab_style(
-            style = cell_text(
+        gt::tab_style(
+            style = gt::cell_text(
                 color = ms_brand_palette("main")[length(ms_brand_palette("main"))],
                 weight = 'bold',
-                font = google_font('Merriweather')
+                font = gt::google_font('Merriweather')
             ),
-            locations = cells_title(groups = 'title')
+            locations = gt::cells_title(groups = 'title')
         ) |>
-        tab_style(
-            style = cell_fill(color = 'grey90'),
+        gt::tab_style(
+            style = gt::cell_fill(color = 'grey90'),
             # Use number of rows from data set
-            locations = cells_body(rows = seq(1, n_rows, 2))
+            locations = gt::cells_body(rows = seq(1, n_rows, 2))
         )
 }
